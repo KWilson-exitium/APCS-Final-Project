ced91f6de1e0e797eb294862973a6037ae44ae5b
Joseph Stalin <schroderowen@gmail.com>
2016-06-13 12:47:48 -0700

    Add files via upload

---
+++ KeyTest.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+
+public class KeyTest implements ActionListener, KeyListener{
+   public KeyTest() {
+   }  
+   String let;
+   public void keyPressed(KeyEvent e){
+      if(e.getKeyCode() == KeyEvent.VK_W) {
+         let = "W";
+      }
+      if(e.getKeyCode() == KeyEvent.VK_A) {
+         let = "A";
+      }
+      if(e.getKeyCode() == KeyEvent.VK_S) {
+         let = "S";
+      }
+      if(e.getKeyCode() == KeyEvent.VK_D) {
+         let = "D";
+      }
+      if(e.getKeyCode() == KeyEvent.VK_E) {
+         let = "E";
+      }
+   }   
+   public boolean key(String button) {    
+      boolean pressed = false;
+      if(button.equals(let)) {
+         pressed = true;
+      }     
+      return pressed;
+   }
+   public void actionPerformed(ActionEvent e) {
+   }
+   public void keyReleased(KeyEvent e){
+   }
+
+   public void keyTyped(KeyEvent e){
+   
+   }
+}
