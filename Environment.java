6518980f9677fcbc748ef5c4a47222950239f89a
Joseph Stalin <schroderowen@gmail.com>
2016-06-13 16:12:32 -0700

    Add files via upload

--- Environment.java
+++ Environment.java
@@ -1,7 +1,12 @@
 import java.io.*;
 import java.util.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-public class Environment{
+public class Environment extends Map{
+   Map level = new Map();
    //This block handles Items
    /*int[] itemXY(){
       int[] xy = new int[2];
@@ -18,6 +23,37 @@ public class Environment{
       }
       return gotKey;
    }
-   //This block handles Map Generation
+  
+   public boolean isWall(int x, int y, String direction) throws IOException{
+      BufferedImage room = ImageIO.read(new File(level.getX() + "," + level.getY() + ".png"));   
+      int[][] walls = convert(room);   
+      boolean blocked = false;
+      if(direction.equals("UP")) {
+         if(walls[y-1][x] == -3584){blocked = true;}
+      }
+      if(direction.equals("DOWN")) {
+         if(walls[y=1][x] == -3584){blocked = true;}
+      }
+      if(direction.equals("LEFT")) {
+         if(walls[y][x-1] == -3584){blocked = true;}
+      }
+      if(direction.equals("RIGHT")) {
+         if(walls[y][x+1] == -3584){blocked = true;}
+      }
+      return true;      
+   } 
+    
+   static int[][] convert(BufferedImage image) {
+      int width = image.getWidth();
+      int height = image.getHeight();
+      int[][] result = new int[height][width];
    
+      for (int row = 0; row < height; row++) {
+         for (int col = 0; col < width; col++) {
+            result[row][col] = image.getRGB(col, row);
+         }
+      }
+   
+      return result;
+   }
 }
