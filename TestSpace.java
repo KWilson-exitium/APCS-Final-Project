351ff529fc8ecfae1f3911beb63387539c7190f5
Kevin Wilson <iwilson.exitium@gmail.com>
2016-06-13 15:53:29 -0700

    Commented out itemTest code and worked on MapTest
    itemTest commented out because i'm not working on it at present.

--- TestSpace.java
+++ TestSpace.java
@@ -2,13 +2,16 @@ import java.util.*;
 import java.awt.*;
 import java.io.*;
 public class TestSpace{
-   void main(String[] args)throws FileNotFoundException , IOException{
+   public static void main(String[] args)throws FileNotFoundException , IOException{
       DrawingPanel gameWindow = new DrawingPanel(800,500);
       Graphics g = gameWindow.getGraphics();
       Scanner Console;
+      KeyTest keyPressed = new KeyTest();
+      Map Layout = new Map();
+      MapTest(g , Layout , false , keyPressed);
       //ItemTest(Console , g);
    }
-   void ItemTest(Scanner Console, Graphics g)throws IOException{
+   /*void ItemTest(Scanner Console, Graphics g)throws IOException{
       //System.out.println(Item.getName());
       Console = new Scanner(new File("0,0_config.txt"));
       ArrayList<Item> itemLister = new ArrayList();
@@ -22,11 +25,30 @@ public class TestSpace{
       Kevin.drawAt(30 , 80 , "Bacon.jpg", Console , g);
       //Item.moveWith(true);
       //Item.move(1 , 30 , 80);  
-   }
+   }*/
    void PlayerTest(Graphics g){
       
    }
-   void MapTest(Scanner Console , Graphics g)throws IOException{
-      
-   } 
+   public static void MapTest(Graphics g , Map Layout , boolean endMet , KeyTest keyPressed)throws IOException{
+      int x = 0;
+      int y = 1;
+      long lastTime = System.nanoTime();
+      double amountOfTicks = 60.0;
+      double ns = 1000000000;
+      double delta = 0;
+      long timer = System.currentTimeMillis();
+      int frames = 0;
+      while(!endMet){
+         long now = System.nanoTime();
+         delta += (now - lastTime) / ns;
+         lastTime = now;
+         if (keyPressed.key("W")){
+            y++;
+         }
+         Layout.drawRoom(x , y , g);
+         if(x == 0 && y == 2){
+            endMet = true;
+         }
+      }  
+   }
 }
